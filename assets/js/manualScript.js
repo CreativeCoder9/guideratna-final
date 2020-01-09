b7e676574b680811f7ac5fedecc59c7de2cb63da
CreativeCoder9 <51862350+CreativeCoder9@users.noreply.github.com>
2020-01-09 11:39:00 +0530

    Working on seo

--- assets/js/manualScript.js
+++ assets/js/manualScript.js
@@ -1,32 +1,32 @@
 
-        var images = ["Bg1.jpg","Bg2.jpg","Bg3.jpg"];
-            var i = 0; 			// Start Point
-            var time = 3000;	// Time Between Switch
-    //         $('#bgchange')
-    //   .css('background-image', 'url(' + settings.images[settings.current] + ')')
-    //   .css('-webkit-transition', + settings.transitionTime + 's ease-in-out')
-    //   .css('-moz-transition', + settings.transitionTime + 'ms ease-in-out')
-    //   .css('-ms-transition', + settings.transitionTime + 'ms ease-in-out')
-    //   .css('-o-transition', + settings.transitionTime + 'ms ease-in-out')
-    //   .css('transition', + settings.transitionTime + 'ms ease-in-out')
-            function changeImg(){
-            
-                $("#bgchange").css("background-image", "url(images/" + images[i] + ")" );
+    //     var images = ["Bg1.jpg","Bg2.jpg","Bg3.jpg"];
+    //         var i = 0; 			// Start Point
+    //         var time = 3000;	// Time Between Switch
+    // //         $('#bgchange')
+    // //   .css('background-image', 'url(' + settings.images[settings.current] + ')')
+    // //   .css('-webkit-transition', + settings.transitionTime + 's ease-in-out')
+    // //   .css('-moz-transition', + settings.transitionTime + 'ms ease-in-out')
+    // //   .css('-ms-transition', + settings.transitionTime + 'ms ease-in-out')
+    // //   .css('-o-transition', + settings.transitionTime + 'ms ease-in-out')
+    // //   .css('transition', + settings.transitionTime + 'ms ease-in-out')
+    //         function changeImg(){
+
+    //             $("#bgchange").css("background-image", "url(images/" + images[i] + ")" );
 
 
 
-                // Check If Index Is Under Max
-                if(i < images.length - 1){
-                // Add 1 to Index
-                i++; 
-                } else { 
-                    // Reset Back To O
-                    i = 0;
-                }
+    //             // Check If Index Is Under Max
+    //             if(i < images.length - 1){
+    //             // Add 1 to Index
+    //             i++; 
+    //             } else { 
+    //                 // Reset Back To O
+    //                 i = 0;
+    //             }
 
-                // Run function every x seconds
-                setTimeout("changeImg()", 5000);
-            }
+    //             // Run function every x seconds
+    //             setTimeout("changeImg()", 5000);
+    //         }
 
-            // Run function when page loads
-            window.onload=changeImg;
+    //         // Run function when page loads
+    //         window.onload=changeImg;
